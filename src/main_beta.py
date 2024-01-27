642ba68aed651fe46cdab5dd2b0a5208ba34cebb
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-01-27 21:25:30 +0100

    Update main_beta.py

--- src/main_beta.py
+++ src/main_beta.py
@@ -22,7 +22,7 @@ token = getenv("TOKEN")
 async def on_ready() -> None:
     global uptime
     print(bot.user.name, "is ready")
-    uptime = round(time.time())
+    uptime = round(time.time())        #mettre la variable uptime au timestamp de demarage 
 
 @bot.command(name="say")
 @commands.has_permissions(administrator = True)
@@ -30,16 +30,25 @@ async def say(ctx, *content: str) -> None:
   await ctx.message.delete()
   await ctx.channel.send(" ".join(content))
 
-@bot.command(name= "status")
-async def ping(ctx):
+
+#@bot.command(name= "status")
+#async def status(ctx):
+   #global uptime
+   #await ctx.message.delete()    #supp le msg de la commande
+   #print(f"Test request by : {ctx.author} (commands = status)") #ecrire dans la console qui a fait la commande
+   #embed = Embed(title = "Status", description = f"{bot.user.name} ping is {round(bot.latency)*1000} ms |  Uptime : <t:{uptime}:R>" , color = 0x71368A) #affiche le ping + depuis qd le bot est on dans un embed
+   #embed.set_thumbnail(url = ctx.guild.icon_url)
+   #embed.set_footer(text = f"Requested by {ctx.author}", icon_url = ctx.author.avatar_url)
+   #await ctx.reply(embed = embed) #envoyer  l'embed
+   #await ctx.channel.send(f"Ping author : {ctx.author.mention}")  #ping le mec qui a fait la commande
+  
+@bot.command(name = "status")
+async def status(ctx):
    global uptime
-   await ctx.message.delete()
-   print(f"Test request by : {ctx.author} (commands = status)")
-   embed = Embed(title = "Status", description = f"{bot.user.name} ping is {round(bot.latency)*1000} ms |  Uptime : <t:{uptime}:R>" , color = 0x71368A)
-   embed.set_thumbnail(url = ctx.guild.icon_url)
-   embed.set_footer(text = f"Requested by {ctx.author}", icon_url = ctx.author.avatar_url)
-   await ctx.reply(embed = embed)
-   await ctx.send(f"Ping author : {ctx.author.mention}")
+   await ctx.message.delete()    #supp le msg de la commande
+   print(f"Test request by : {ctx.author} (commands = status)") #ecrire dans la console qui a fait la commande
+   await ctx.channel.send(f"{bot.user.name} ping is {round(bot.latency)*1000} ms |  Uptime : <t:{uptime}:R>")
+   await ctx.channel.send(f"Requested by : {ctx.author.mention}")  #ping le mec qui a fait la commande
 
 bot.run(token)
 
