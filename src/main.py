74399a9a3ed3f19b2df8cd770dd3cd5927bc8565
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-01-28 19:26:32 +0100

    py-cord

--- src/main.py
+++ src/main.py
@@ -1,24 +1,27 @@
-from sys import argv
 from json import loads as json_loads
-from discord import Intents
+
+import discord
 from discord.ext import commands
 
-path = argv[0][:len(argv[0]) - list(reversed(list(argv[0]))).index("/") - 1]
-
-file = open(path + "/../config.json", "r")
+file = open("config.json", "r")
 config = json_loads(file.read())
 
-bot = commands.Bot(command_prefix=config.get("prefix"), intents=Intents.all())
-token = getenv("TOKEN")
+bot = commands.Bot()
 
 @bot.event
 async def on_ready() -> None:
-    print(bot.user.name, "is ready")
+  print(bot.user.name, "is ready")
 
-@bot.command(name="say")
+@bot.slash_command(
+  name = "dire",
+  description = "Fais dire quelque chose au robot."
+)
 @commands.has_permissions(administrator = True)
-async def say(ctx, *content: str) -> None:
-  await ctx.message.delete()
-  await ctx.channel.send(" ".join(content))
+async def say(
+  ctx: discord.application_command(), 
+  message: discord.Option(str)
+) -> None:
+  await ctx.delete()
+  await ctx.channel.send(message)
 
 bot.run(config.get("token"))
