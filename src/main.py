1d7cc458dfbf4391727d79473d2ef90ec249fe0e
switchcodeur <switchcodeur@mail.com>
2024-01-28 14:07:05 +0100

    Removed docker

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
-from dotenv import load_dotenv
-from os import getenv
 from sys import argv
 from json import loads as json_loads
 from discord import Intents
 from discord.ext import commands
 
-load_dotenv() 
 path = argv[0][:len(argv[0]) - list(reversed(list(argv[0]))).index("/") - 1]
 
 file = open(path + "/../config.json", "r")
@@ -24,4 +21,4 @@ async def say(ctx, *content: str) -> None:
   await ctx.message.delete()
   await ctx.channel.send(" ".join(content))
 
-bot.run(token)
+bot.run(config.get("token"))
