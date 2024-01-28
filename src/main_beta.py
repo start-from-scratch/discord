7776114cfb7251f82c5e49435d41befad248a8c1
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-01-28 19:47:32 +0100

    New main_beta | add py-cord

--- src/main_beta.py
+++ src/main_beta.py
@@ -1,22 +1,15 @@
-from dotenv import load_dotenv
-from os import getenv
-from sys import argv
-from json import loads as json_loads
 from discord import Intents, Embed
 from discord.ext import commands
 import time
+from json import loads as json_loads
 
+file = open("config.json", "r")
+config = json_loads(file.read())
+
+bot = commands.Bot()
 
 uptime = 0
 
-load_dotenv() 
-path = argv[0][:len(argv[0]) - list(reversed(list(argv[0]))).index("/") - 1]
-
-file = open(path + "/../config.json", "r")
-config = json_loads(file.read())
-
-bot = commands.Bot(command_prefix=config.get("prefix"), intents=Intents.all())
-token = getenv("TOKEN")
 
 @bot.event
 async def on_ready() -> None:
@@ -25,13 +18,22 @@ async def on_ready() -> None:
     uptime = round(time.time())        #mettre la variable uptime au timestamp de demarage 
     await bot.get_channel(1201190228233310248).send(f"Bot {bot.user.mention} demarré :green_circle: ") #mettre votre id de salon ou va etre envoyer quand le bot est allumé
 
-@bot.command(name="say")
+@bot.slash_command(
+  name = "say",
+  description = "Fais dire quelque chose au bot."
+)
 @commands.has_permissions(administrator = True)
-async def say(ctx, *content: str) -> None:
-  await ctx.message.delete()
-  await ctx.channel.send(" ".join(content))
+async def say(
+  ctx: discord.application_command(), 
+  message: discord.Option(str)
+) -> None:
+  await ctx.delete()
+  await ctx.channel.send(message)
   
-@bot.command(name = "status")
+@bot.slash_command(
+    name = "status" 
+    description = "Avoir des Informations sur le bot" 
+ )
 @commands.has_permissions(administrator = True)
 async def status(ctx):
     global uptime
