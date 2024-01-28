6bd7c41e46aba5228161a6c3024af540b84132d2
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-01-28 16:44:47 +0100

    Update main_beta.py

--- src/main_beta.py
+++ src/main_beta.py
@@ -23,34 +23,25 @@ async def on_ready() -> None:
     global uptime
     print(bot.user.name, "is ready")
     uptime = round(time.time())        #mettre la variable uptime au timestamp de demarage 
+    await bot.get_channel(1201190228233310248).send(f"Bot {bot.user.mention} demarré ") #mettre votre id de salon ou va etre envoyer quand le bot est allumé
 
 @bot.command(name="say")
 @commands.has_permissions(administrator = True)
 async def say(ctx, *content: str) -> None:
   await ctx.message.delete()
   await ctx.channel.send(" ".join(content))
-
-
-#@bot.command(name= "status")
-#async def status(ctx):
-   #global uptime
-   #await ctx.message.delete()    #supp le msg de la commande
-   #print(f"Test request by : {ctx.author} (commands = status)") #ecrire dans la console qui a fait la commande
-   #embed = Embed(title = "Status", description = f"{bot.user.name} ping is {round(bot.latency)*1000} ms |  Uptime : <t:{uptime}:R>" , color = 0x71368A) #affiche le ping + depuis qd le bot est on dans un embed
-   #embed.set_thumbnail(url = ctx.guild.icon_url)
-   #embed.set_footer(text = f"Requested by {ctx.author}", icon_url = ctx.author.avatar_url)
-   #await ctx.reply(embed = embed) #envoyer  l'embed
-   #await ctx.channel.send(f"Ping author : {ctx.author.mention}")  #ping le mec qui a fait la commande
   
 @bot.command(name = "status")
 @commands.has_permissions(administrator = True)
 async def status(ctx):
     global uptime
-    ping = round((time.time() - ctx.message.created_at.timestamp()) * 1000)
     await ctx.message.delete()    # supprimer le message de la commande
     print(f"Test request by: {ctx.author} in {ctx.guild.name}") # écrire dans la console qui a fait la commande
-    await ctx.channel.send(f"{bot.user.name} ping is {ping} ms | Uptime: <t:{uptime}:R>")
+    await ctx.channel.send(f"{bot.user.name} ping is {ping(ctx.message.created_at.timestamp())} ms | Uptime: <t:{uptime}:R>")
     await ctx.channel.send(f"Requested by: {ctx.author.mention}")  # ping le mec qui a fait la commande
 
+
+def ping(message_timestamp):
+     return round((time.time() - message_timestamp) * 1000)
 bot.run(token)
 
