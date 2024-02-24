296e6dea69c9afbc1dcafb2ce3c90df2cef07c68
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-02-24 12:19:43 +0100

    Update main_beta.py

--- src/main_beta.py
+++ src/main_beta.py
@@ -1,4 +1,5 @@
-from discord import application_command, Option, Embed, Color
+#from discord import application_command, Option, Embed, Color
+import discord
 from discord.ext import commands
 from logs import logger
 from time import time
