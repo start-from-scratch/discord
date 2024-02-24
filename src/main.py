e68de34068c1b44694422260252c3492d6bfed3d
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-02-24 11:46:35 +0100

    New Version

--- src/main.py
+++ src/main.py
@@ -11,12 +11,21 @@ with open("id.txt", "r") as f:
   id = int(f.read())
   f.close()
   
-start = int(time()) #voir uptime 
+start = int(time())          #voir uptime 
 bot = commands.Bot()
 
 @bot.event
 async def on_ready() -> None:
-    await bot.get_channel(id).send(f"Bot {bot.user.mention} demarré :green_circle:") #envoie un msg dans le salon id au demarrage du bot
+    await bot.get_channel(id).send(f"Bot {bot.user.mention} demarré :green_circle: (Version Beta)") #envoie un msg dans le salon id au demarrage du bot
+
+@bot.event
+async def on_message_delete(message):
+    if message.mentions:
+        for user in message.mentions:
+            channel = bot.get_channel(message.channel.id)
+            await user.send(f"Vous avez été ghost ping par {message.author.name} dans le salon {channel.name} du serveur {message.guild.name}")
+            await channel.send(f"{user.mention} vous avez été ghost ping par {message.author.mention}")
+
 
 @bot.slash_command(
   name = "say",
@@ -31,13 +40,25 @@ async def say(
   await ctx.channel.send(message)
 
 @bot.slash_command(
-    name = "status",
+    name = "infos",
     description = "Avoir des Informations sur le bot" 
 )
-@commands.has_permissions(administrator = True)
+async def infos(ctx):
+    await ctx.respond(f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms \n A été lancé <t:{start}:R> \n Actuellement dans {len(bot.guilds)} serveurs \n Requested by: {ctx.author.mention}")
+
+@bot.slash_command(
+    name = "ping",
+    description = "Avoir le ping du bot" 
+)
 async def status(ctx):
-    ping = round(bot.latency * 1000)
-    await ctx.respond(f"{bot.user.mention} ping is {ping} ms | A été lancé <t:{start}:R> \n Requested by: {ctx.author.mention}")
+    await ctx.respond(f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms")
+
+@bot.slash_command(
+    name = "help",
+    description = "Liste des commandes disponibles" 
+)
+async def help(ctx):
+    await ctx.respond(f"Commandes Disponible : \n `/ping` - Avoir le ping du bot \n `/infos` - Avoir des Informations sur le bot \n `/help` - Liste des commandes disponibles \n `/say` - Fais dire quelque chose au bot (admin only)")
 
 
 bot.run(token)
