8e67a0f2651e133ddf6592bd10dbe873011d53c1
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-02-25 00:54:36 +0100

    Update embed

--- src/main_beta.py
+++ src/main_beta.py
@@ -19,8 +19,8 @@ bot = commands.Bot()
 def create_embed(titre, description, auteur, auteur_url):
     embed = discord.Embed(title=titre, description=description, color=discord.Color.random())
     #embed.add_field(name="Champ 1", value="Valeur 1")
-    embed.set_footer(text=f"Requested by:{auteur}")
-    embed.set_thumbnail(url=auteur_url)
+    embed.set_author(name= auteur, icon_url= auteur_url )
+    #embed.set_thumbnail(url=auteur_url)
     #embed.set_image(url=auteur_url)
     #embed.set_author(name=auteur)
     return embed
@@ -79,7 +79,7 @@ async def embed(ctx,
   titre: Option(str), 
   description: Option(str)
 ):
-  embed = create_embed(titre, description, ctx.author.name, ctx.author.avatar_url)
+  embed = create_embed(titre, description, ctx.author.display_name, ctx.author.avatar_url)
   await ctx.respond(embed=embed)
 
 
