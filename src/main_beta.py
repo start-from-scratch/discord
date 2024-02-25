552654d81fc97df90cc2b2378842de4192a5698a
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-02-25 13:07:14 +0100

    Update embed creator

--- src/main_beta.py
+++ src/main_beta.py
@@ -16,8 +16,8 @@ start = int(time())          #voir uptime
 bot = commands.Bot()
 
 
-def create_embed(titre, description, auteur):
-    embed = discord.Embed(title=titre, description=description, color=discord.Color.random())
+def create_embed(titre, description, auteur,couleur ):
+    embed = discord.Embed(title=titre, description=description, color=couleur)
     #embed.add_field(name="Champ 1", value="Valeur 1")
     #embed.set_author(name= auteur)
     #embed.set_thumbnail(url=auteur_url)
@@ -27,18 +27,17 @@ def create_embed(titre, description, auteur):
 @bot.event
 async def on_ready() -> None:
     await bot.get_channel(id).send(f"Bot {bot.user.mention} demarré :green_circle: (Version Beta)") #envoie un msg dans le salon id au demarrage du bot
-    demarrage = 1
 
 @bot.event
 async def on_message_delete(message):
-    if message.mentions:
+    if message.mentions and not message.author.bot:
         for user in message.mentions:
             channel = bot.get_channel(message.channel.id)
             await user.send(f"Vous avez été ghost ping par {message.author.name} dans le salon {channel.name} du serveur {message.guild.name}", )
-            embed = create_embed("Ghost Ping","Un Ghost ping viens d'etre detecte", bot.user.name)
+            embed = create_embed("Ghost Ping","Un Ghost ping viens d'etre detecte", bot.user.name, discord.Color.random())
             embed.add_field(name="Author:", value= message.author.mention, inline = True)
             embed.add_field(name="Mention:", value=user.mention, inline = True)
-            embed.add_field(name="Channel:", value=f"ID: {message.channel.id} \n Name: {message.channel.name}", inline = True)
+            embed.add_field(name="Channel:", value=f"ID: {message.channel.id} \n Name: {message.channel.name}", inline = False)
             await channel.send(embed=embed)
 
                            
@@ -59,7 +58,7 @@ async def say(
     description = "Avoir des Informations sur le bot" 
 )
 async def infos(ctx):
-    embed = create_embed("Infos", f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms \n A été lancé <t:{start}:R> \n Actuellement dans {len(bot.guilds)} serveurs", ctx.author.name)
+    embed = create_embed("Infos", f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms \n A été lancé <t:{start}:R> \n Actuellement dans {len(bot.guilds)} serveurs", ctx.author.name, "#008FFF")
     await ctx.respond(embed=embed)
 
 
@@ -68,7 +67,7 @@ async def infos(ctx):
     description = "Avoir le ping du bot" 
 )
 async def ping(ctx):
-    embed = create_embed("Ping", f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms", ctx.author.name)
+    embed = create_embed("Ping", f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms", ctx.author.name,"#FFA900")
     await ctx.respond(embed=embed)
 
 @bot.slash_command(
@@ -76,7 +75,7 @@ async def ping(ctx):
     description = "Liste des commandes disponibles" 
 )
 async def help(ctx):
-    embed = create_embed("Help", f"Commandes Disponible : \n `/ping` - Avoir le ping du bot \n `/infos` - Avoir des Informations sur le bot \n `/help` - Liste des commandes disponibles \n `/say` - Fais dire quelque chose au bot (admin only)", ctx.author.name)
+    embed = create_embed("Help", f"Commandes Disponible : \n `/ping` - Avoir le ping du bot \n `/infos` - Avoir des Informations sur le bot \n `/help` - Liste des commandes disponibles \n `/say` - Fais dire quelque chose au bot (admin only)", ctx.author.name, "#200B9C")
     await ctx.respond(embed=embed)
 
 @bot.slash_command(
@@ -85,9 +84,10 @@ async def help(ctx):
 )
 async def embed(ctx, 
   titre: Option(str), 
-  description: Option(str)
+  description: Option(str),
+  couleur: Option(str)
 ):
-  embed = create_embed(titre, description, ctx.author.name)
+  embed = create_embed(titre, description, ctx.author.name,couleur)
   await ctx.respond(embed=embed)
 
 
