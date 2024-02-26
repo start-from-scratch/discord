81b97f8cc84cb2ac1ff8cc5a4538bc206e12638e
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-02-26 13:25:04 +0100

    old main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
-import discord
-from discord import application_command, Option, Embed, Color
+from discord import application_command, Option
 from discord.ext import commands
 from logs import logger
 from time import time
@@ -15,35 +14,19 @@ with open("id.txt", "r") as f:
 start = int(time())          #voir uptime 
 bot = commands.Bot()
 
-
-def create_embed(titre, description, auteur, couleur):
-    embed = discord.Embed(title=titre, description=description, color=couleur)
-    #embed.add_field(name="Champ 1", value="Valeur 1")
-    #embed.set_author(name= auteur)
-    #embed.set_thumbnail(url=auteur_url)
-    embed.set_footer(text=f"Information requested by: {auteur}")
-    return embed
-
 @bot.event
 async def on_ready() -> None:
     await bot.get_channel(id).send(f"Bot {bot.user.mention} demarré :green_circle:") #envoie un msg dans le salon id au demarrage du bot
 
 @bot.event
 async def on_message_delete(message):
-  if message.author in message.mentions:
-    return
-  if message.mentions and not message.author.bot:
-          for user in message.mentions:
-            if not user.bot:
-                  channel = bot.get_channel(message.channel.id)
-                  await user.send(f"Vous avez été ghost ping par {message.author.name} dans le salon {channel.name} du serveur {message.guild.name}")
-                  embed = create_embed("Ghost Ping","Un Ghost ping vient d'être détecté", bot.user.name, discord.Color.random())
-                  embed.add_field(name="Author:", value= message.author.mention, inline = True)
-                  embed.add_field(name="Mention:", value=user.mention, inline = True)
-                  embed.add_field(name="Channel:", value=f"ID: {message.channel.id} \n Name: {message.channel.name}", inline = False)
-                  await channel.send(embed=embed) 
+    if message.mentions:
+        for user in message.mentions:
+            channel = bot.get_channel(message.channel.id)
+            await user.send(f"Vous avez été ghost ping par {message.author.name} dans le salon {channel.name} du serveur {message.guild.name}")
+            await channel.send(f"{user.mention} vous avez été ghost ping par {message.author.mention}")
 
-                           
+
 @bot.slash_command(
   name = "say",
   description = "Fais dire quelque chose au bot."
@@ -61,38 +44,21 @@ async def say(
     description = "Avoir des Informations sur le bot" 
 )
 async def infos(ctx):
-    embed = create_embed("Infos", f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms \n A été lancé <t:{start}:R> \n Actuellement dans {len(bot.guilds)} serveurs", ctx.author.name, 0x008FFF)
-    await ctx.respond(embed=embed)
-
+    await ctx.respond(f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms \n A été lancé <t:{start}:R> \n Actuellement dans {len(bot.guilds)} serveurs \n Requested by: {ctx.author.mention}")
 
 @bot.slash_command(
     name = "ping",
     description = "Avoir le ping du bot" 
 )
-async def ping(ctx):
-    embed = create_embed("Ping", f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms", ctx.author.name, 0xFFA900)
-    await ctx.respond(embed=embed)
+async def status(ctx):
+    await ctx.respond(f"{bot.user.mention} ping is {int(bot.latency * 1000)} ms")
 
 @bot.slash_command(
     name = "help",
     description = "Liste des commandes disponibles" 
 )
 async def help(ctx):
-    embed = create_embed("Help", f"Commandes Disponible : \n `/ping` - Avoir le ping du bot \n `/infos` - Avoir des Informations sur le bot \n `/help` - Liste des commandes disponibles \n `/say` - Fais dire quelque chose au bot (admin only) \n `/embed` - Crée un embed", ctx.author.name, 0x200B9C)
-    await ctx.respond(embed=embed)
-
-@bot.slash_command(
-    name= "embed",
-    description="Crée un embed" 
-)
-async def embed(ctx, 
-  titre: Option(str), 
-  description: Option(str),
-):
-  embed = create_embed(titre, description, ctx.author.name,0x093156)
-  await ctx.respond(embed=embed)
-
-
+    await ctx.respond(f"Commandes Disponible : \n `/ping` - Avoir le ping du bot \n `/infos` - Avoir des Informations sur le bot \n `/help` - Liste des commandes disponibles \n `/say` - Fais dire quelque chose au bot (admin only)")
 
 
 bot.run(token)
