5b1827882a021dc0a7e8aab2c955ad92bcb3e7df
loyds44 <127327600+loyds44@users.noreply.github.com>
2024-02-26 13:23:27 +0100

    Ajout commentaire & Update Ghost Ping

--- src/main_beta.py
+++ src/main_beta.py
@@ -15,7 +15,7 @@ with open("id.txt", "r") as f:
 start = int(time())          #voir uptime 
 bot = commands.Bot()
 
-
+#Embed createur
 def create_embed(titre, description, auteur, couleur):
     embed = discord.Embed(title=titre, description=description, color=couleur)
     #embed.add_field(name="Champ 1", value="Valeur 1")
@@ -24,10 +24,12 @@ def create_embed(titre, description, auteur, couleur):
     embed.set_footer(text=f"Informations demandées par : {auteur}")
     return embed
 
+#Demarrage du bot
 @bot.event
 async def on_ready() -> None:
     await bot.get_channel(id).send(f"Bot {bot.user.mention} demarré :green_circle: (Version Beta)") #envoie un msg dans le salon id au demarrage du bot
 
+#Ghost Ping
 @bot.event
 async def on_message_delete(message):
   if message.author in message.mentions:
@@ -43,7 +45,7 @@ async def on_message_delete(message):
                   embed.add_field(name="Salon:", value=f"ID: {message.channel.id} \n Nom: <#{message.channel.id}>", inline = False)
                   await channel.send(embed=embed) 
 
-                           
+#Commande Say                           
 @bot.slash_command(
   name = "say",
   description = "Fais dire quelque chose au bot."
@@ -56,6 +58,7 @@ async def say(
   await ctx.delete()
   await ctx.channel.send(message)
 
+#Commande infos
 @bot.slash_command(
     name = "infos",
     description = "Avoir des Informations sur le bot" 
@@ -64,7 +67,7 @@ async def infos(ctx):
     embed = create_embed("Infos", f"Le ping du bot {bot.user.mention} est de {int(bot.latency * 1000)}ms \n A été lancé <t:{start}:R> \n Actuellement dans {len(bot.guilds)} serveur(s)", ctx.author.name, 0x008FFF)
     await ctx.respond(embed=embed)
 
-
+#Commande ping
 @bot.slash_command(
     name = "ping",
     description = "Avoir le ping du bot" 
@@ -73,6 +76,7 @@ async def ping(ctx):
     embed = create_embed("Ping", f"Le ping du bot {bot.user.mention} est de {int(bot.latency * 1000)}ms", ctx.author.name, 0xFFA900)
     await ctx.respond(embed=embed)
 
+#Commande help
 @bot.slash_command(
     name = "help",
     description = "Liste des commandes disponibles" 
@@ -81,6 +85,7 @@ async def help(ctx):
     embed = create_embed("Help", f"Commandes Disponible : \n `/ping` - Avoir le ping du bot \n `/infos` - Avoir des Informations sur le bot \n `/help` - Liste des commandes disponibles \n `/say` - Fais dire quelque chose au bot (admin only) \n `/embed` - Crée un embed", ctx.author.name, 0x200B9C)
     await ctx.respond(embed=embed)
 
+#Commande embed
 @bot.slash_command(
     name= "embed",
     description="Crée un embed" 
