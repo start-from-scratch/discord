464e7ab0f3eb60043f1719d6de13677dd262b0fb
loyds44 <e.loyds44@gmail.com>
2024-02-27 20:04:54 +0100

    Update main_beta / Patch serveur, infos, ghost ping....

--- src/main_beta.py
+++ src/main_beta.py
@@ -3,10 +3,12 @@ from discord.ext import commands
 from logger import logger
 from datetime import datetime
 from json import load as json_load
+from time import time
 
 config = json_load(open("config.json", "r"))
   
-start = datetime.now()
+start = int(time())  
+start_date = datetime.fromtimestamp(start)
 bot = commands.Bot(intents = Intents.all())
 bot.remove_command("help")
 
@@ -20,12 +22,12 @@ async def on_ready() -> None:
   logger.info(f"Logged in as {bot.user.name}.")
 
   for channel_id in config["status_channel_id"]:
-    await bot.get_channel(channel_id).send(f"Le Pixelbot est connecté en tant que {bot.user.mention}. :green_circle:")
+    await bot.get_channel(channel_id).send(f"Le bot est connecté en tant que {bot.user.mention} :green_circle: (Version Beta)")
 
   await bot.change_presence(
     activity = Activity(
-      type = ActivityType.listening,
-      name = "death metal"
+      type = ActivityType.watching,
+      name = f"{len(bot.guilds)} serveurs"
     )
   )
 
@@ -42,7 +44,7 @@ async def on_message_delete(ctx: Message) -> None:
   embed = Embed(title = "Ghost ping", timestamp = ctx.created_at, colour = Colour.random())
   embed.add_field(name = "Auteur" , value = ctx.author.mention)
   embed.add_field(name = "Message", value = ctx.content)
-
+  embed.add_field(name="Salon:", value=f"ID: {ctx.channel.id} \n Nom: <#{ctx.channel.id}>", inline = False)
   await ctx.channel.send(embed = embed)
 
 @bot.slash_command(
@@ -98,7 +100,8 @@ async def embed(
   description="Avoir des informations sur le serveur" 
 )
 async def serveur(ctx: Message) -> None:
-  embed = create_embed("Infos serveur", f"Crée le {datetime.fromtimestamp(ctx.guild.created_at.timestamp())} \n Nombre de Membres: {ctx.guild.member_count} \n Nombre de salon textuels: {len(ctx.guild.text_channels)}, Nombre de salon vocaux: {len(ctx.guild.voice_channels)} \n Niveau de Boost:{guild_object.premium_subscription_count} | Booster:{guild_object.premium_subscribers} \n Roles: {ctx.guild.roles}", ctx.author.name,0x1DB747)
+  roles_list = " | ".join((f"<@&{role.id}>" for role in ctx.guild.roles))
+  embed = create_embed(f"Infos du serveur {ctx.guild.name}", f"Crée le {ctx.guild.created_at} \n Nombre de Membres: {ctx.guild.member_count} \n Proprietaire : {ctx.guild.owner} \n Nombre de salon textuels: {len(ctx.guild.text_channels)}, Nombre de salon vocaux: {len(ctx.guild.voice_channels)}, Nombre de Roles: {len(ctx.guild.roles)} \n Roles: {roles_list}", ctx.author.name,0x1DB747)
   await ctx.respond(embed=embed)
 
 bot.run(config["token"])
