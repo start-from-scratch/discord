7808f4d3b8d6ec2d6a4f7a3ed0571baf779f078d
loyds44 <e.loyds44@gmail.com>
2024-02-27 16:44:48 +0100

    Update

--- src/logger.py
+++ src/logger.py
@@ -1,17 +1,9 @@
-from logging import getLogger, FileHandler, Formatter, INFO
+from logging import getLogger, FileHandler, Formatter, DEBUG
 from rich.logging import RichHandler
-
-f_handler = FileHandler(
-    filename = "discord.log",
-    encoding = "utf-8",
-    mode = "a"
-)
-f_handler.setFormatter(Formatter("%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s"))
 
 r_handler = RichHandler()
 r_handler.setFormatter(Formatter("%(message)s"))
 
 logger = getLogger()
-logger.setLevel(INFO)
-logger.addHandler(f_handler)
+logger.setLevel(DEBUG)
 logger.addHandler(r_handler)
